71e451d393a4339a994721f7141c8b24dccf5a98
arman8808 <armanal3066@gmail.com>
2025-06-21 11:07:25 +0530

    added a home route to check server status

--- app.js
+++ app.js
@@ -6,52 +6,41 @@
 // const cors = require('cors')
 // const registrationRoutes = require('./routes/registration')
 
-
-
-
 // app.use(cors())
 // app.use(express.json())
 // app.use(express.urlencoded({extended:true}))
 
-
-
 // app.use('/api/registration', registrationRoutes)
-
-
-
-
 
 // module.exports = app;
 
-
-
-
-
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
+const cors = require("cors");
+const path = require("path");
 
 // Import your routes
-const registrationRoutes = require('./routes/registration');
-const contactRoutes = require('./routes/contact')
+const registrationRoutes = require("./routes/registration");
+const contactRoutes = require("./routes/contact");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Base API URL
-const BASE_API_URL = '/api';
+const BASE_API_URL = "/api";
 
 // Use base URL for all routes
 app.use(`${BASE_API_URL}/registration`, registrationRoutes);
 app.use(`${BASE_API_URL}`, contactRoutes);
-
+app.get("/", (req, res) =>
+  res.status(200).json({ message: "Server is running" })
+);
 
 module.exports = app;
